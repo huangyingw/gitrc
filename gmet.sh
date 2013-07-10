d97155630d682b67164639bf880060a5384ab4f4
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-10 11:12:06 -0700

    update this file, need to be carefully tested

--- gmet.sh
+++ gmet.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 CURRENT_BRANCH="`git branch |awk '/^\*/{print $2}'`"
 TARGET_BRANCH="$1"
-git checkout "$TARGET_BRANCH" \
+git add . \
+  && git stash|grep -q "HEAD is now at" \
+  && git checkout "$TARGET_BRANCH" \
   && git merge "$CURRENT_BRANCH" \
-  && git checkout "$CURRENT_BRANCH"
+  && git checkout "$CURRENT_BRANCH" \
+  && git stash pop stash@{0} \
+  && git reset HEAD . 
