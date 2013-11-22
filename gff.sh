dc53872b701bae90536ab1e285be8928637726e1
huangyingw@linux_programming <huangyingw@gmail.com>
2013-11-22 14:27:21 -0800

    n

--- gff.sh
+++ gff.sh
@@ -2,6 +2,13 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
+
+if [ -z "$1" ];
+then
+  echo -e "${red}Please provide the file name in full ... ${NC}"
+  exit 1
+fi
+
 commit=`git log --all -- "$1"|awk 'NR==1{print $2}'`
 if [ -z "$commit" ];
 then
