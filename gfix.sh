d273e6ddba96f3fefee61093e4096de5747a34d0
huangyingw@linux_programming <huangyingw@gmail.com>
2013-11-25 14:46:34 -0800

    n

--- gfix.sh
+++ gfix.sh
@@ -2,6 +2,6 @@
 current_branch=`git branch |awk '/^\*/{print $2}'`
 fix_branch=`git branch |awk '/^\*/{print $2}'`.fix
 echo $fix_branch
-git branch -D $fix_branch
-~/gitrc/gcia.sh "before initiating new branch $fix_branch from $current_branch"
-git checkout -b $fix_branch
+git branch -d $fix_branch \
+  && ~/gitrc/gcia.sh "before initiating new branch $fix_branch from $current_branch" \
+  && git checkout -b $fix_branch
