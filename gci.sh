423c3cd35f47684c53121d2b138b1f0a79af9865
huangyingw@linux_programming <huangyingw@gmail.com>
2013-12-10 10:36:58 -0800

    n

---
+++ gci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
+
+~/gitrc/gps.sh
+if [ -z "$1" ];
+then
+    echo -e "${red}Please provide commit message ... ${NC}"
+    exit 1
+fi
+git commit -am "$1" && git gc
+~/gitrc/gps.sh              
