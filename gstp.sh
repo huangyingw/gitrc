9ae5c300997c7246757d1077024b91ded4cf94c7
huangyingw@linux_programming <huangyingw@gmail.com>
2013-12-11 14:07:16 -0800

    n

--- gstp.sh
+++ gstp.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-if [ -z "$1" ];
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
+if ! (git status|grep -q 'nothing to commit')
 then
-    git stash pop
-else
-    git stash pop stash@{"$1"}
+  echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
+  exit 0
 fi
+top_branch=`git stash list|awk 'NR==1{print $4}'|sed 's/://g'`
+git co $top_branch
+git stash pop
