1bcde8dcf2e8fb2794edbf8e4b993bbc9826a46f
huangyingw@linux_programming <huangyingw@gmail.com>
2013-12-12 11:03:59 -0800

    n

--- g.sh
+++ g.sh
@@ -4,10 +4,10 @@ green='\033[0;32m'
 NC='\033[0;0m' # No Color
 
 ~/gitrc/gps.sh
-if [ -z "$1" ];
+commit_message='n'
+if [ -n "$1" ];
 then
-    echo -e "${red}Please provide commit message ... ${NC}"
-    exit 1
+  $commit_message="$1"
 fi
-git commit  --no-verify -m "$1" && git gc
+git commit  --no-verify -m "$commit_message" && git gc
 ~/gitrc/gps.sh
