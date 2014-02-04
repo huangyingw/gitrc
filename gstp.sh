00e2f98ebb26968b9b1f549d0b9bef16612e77df
huangyingw@linux_programming <huangyingw@gmail.com>
2014-02-04 15:38:43 -0800

    before initiating new branch gmfix from dev,n

--- gstp.sh
+++ gstp.sh
@@ -2,6 +2,11 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
+if [ -n "$1" ];
+then
+  git stash pop stash@{"$1"}
+  exit 0
+fi
 if ! (git status|grep -q 'nothing to commit')
 then
   echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
@@ -11,9 +16,4 @@ top_branch=`git stash list|awk 'NR==v1 {print $4}' v1=$(($1+1)) |sed 's/://g'`
 echo $top_branch
 git branch |awk '/^\*/{print $2}'
 git co $top_branch
-if [ -n "$1" ];
-then
-  git stash pop stash@{"$1"}
-else
-  git stash pop
-fi
+git stash pop
