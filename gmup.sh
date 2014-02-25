54727094f11c01099fb01280eaf6105c472146bb
huangyingw@linux_programming <huangyingw@gmail.com>
2014-02-25 14:17:59 -0800

    n

--- gmup.sh
+++ gmup.sh
@@ -6,6 +6,11 @@ current_branch=`git branch |awk '/^\*/{print $2}'`
 up_branch=`echo ${current_branch} | sed 's/\(\.[^.]*\)$//'`
 echo $current_branch
 echo $up_branch
+if [ $current_branch == $up_branch ];
+then
+  echo -e "${red}It is already top branch ... ${NC}"
+  exit 1
+fi
 git checkout $up_branch \
   && git merge $current_branch \
   && ~/gitrc/gcia.sh "merge from $current_branch to $up_branch"
