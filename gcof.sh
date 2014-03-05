3ec4a244e0ac527b0ce11a0b69ae9281e71bdcad
huangyingw@linux_programming <huangyingw@gmail.com>
2014-03-05 10:49:19 -0800

    n

--- gcof.sh
+++ gcof.sh
@@ -2,4 +2,4 @@
 # this is used to reset back to the latest change on the file.
 commit=`git log --all -- "$1" |awk 'NR==1{print $2}'`
 cp -fv "$1" "$1".bak \
-  && git checkout HEAD -- "$1"
+  ; git checkout $commit -- "$1"
