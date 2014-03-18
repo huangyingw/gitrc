9f93ae9b84613b395ccee54c22bf9a503bb6fa2a
huangyingw@linux_programming <huangyingw@gmail.com>
2014-03-18 15:16:11 -0700

    n

--- gcof.sh
+++ gcof.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # this is used to reset back to the latest change on the file.
-commit=`git rev-list -n 1 HEAD -- "$1"`
+commit=`git rev-list --branches -n 1 HEAD -- "$1"`
 git checkout $commit^ -- "$1"
