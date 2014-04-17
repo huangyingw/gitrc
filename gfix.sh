db1c313a2b64c7f271fa3a2a2dde6556202e250c
huangyingw@linux_programming <huangyingw@gmail.com>
2014-04-16 22:45:00 -0700

    n

--- gfix.sh
+++ gfix.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
 current_branch=`git branch |awk '/^\*/{print $2}'`
+if  ( echo $current_branch|grep -q 'fix$' )
+then
+  echo -e "${red}gfix should not be run in fix branch ... ${NC}"
+  exit 1
+fi 
 fix_branch=`git branch |awk '/^\*/{print $2}'`.fix
 echo $fix_branch
 git checkout HEAD \
