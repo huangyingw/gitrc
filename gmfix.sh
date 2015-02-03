fee0310a24adb8229ddf61bc70ca695418217915
huangyingw@linux_programming <huangyingw@gmail.com>
2015-02-03 11:25:24 -0800

    n

--- gmfix.sh
+++ gmfix.sh
@@ -20,4 +20,4 @@ git checkout $previous_branch \
   && git merge $fix_branch \
   && git branch -d $fix_branch \
   && for ss in `git remote -v |awk '/\(fetch\)$/{print $1}'`; do git push $ss :$fix_branch; done
-#rm gfvd.already
+rm gfvd.already
