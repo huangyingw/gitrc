f2b48ba523e612cf83feffdec9c9b39ac38f3381
huangyingw@linux_programming <huangyingw@gmail.com>
2015-04-01 14:54:57 -0700

    n

--- g.sh
+++ g.sh
@@ -3,11 +3,11 @@ red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
 
-~/gitrc/gps.sh
+~/loadrc/gitrc/gps.sh
 commit_message='n'
 if [ -n "$1" ];
 then
   commit_message="$1"
 fi
 git commit  --no-verify -m "$commit_message"
-~/gitrc/gps.sh
+~/loadrc/gitrc/gps.sh
